cc61daea9a1e112d0f6657a7da02508c5be25767
putra samb <putra@samb.co.id>
2025-10-14 11:52:07 +0700

    feat: Update CSRF trusted origins for enhanced security

--- core/settings.py
+++ core/settings.py
@@ -280,27 +280,14 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     SECURE_HSTS_PRELOAD = True
 
-# CSRF Configuration
-CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://tanipintar.unklab.id",
+    "https://dev-api.taniverse.id",
+    "https://dev.api.taniverse.id",
     "https://api-corebackend.kancralabs.com",
     "https://kancralabs.com",
-])
-# if env('CSRF_COOKIE_SECURE', default=False):
-#     CSRF_COOKIE_SECURE = True
-# if env('CSRF_COOKIE_HTTPONLY', default=False):
-#     CSRF_COOKIE_HTTPONLY = True
-# if env('CSRF_COOKIE_SAMESITE', default=None):
-#     CSRF_COOKIE_SAMESITE = env('CSRF_COOKIE_SAMESITE')
-# if env('CSRF_USE_SESSIONS', default=False):
-#     CSRF_USE_SESSIONS = True
-
-# Session security configuration
-# if env('SESSION_COOKIE_SECURE', default=False):
-#     SESSION_COOKIE_SECURE = True
-# if env('SESSION_COOKIE_HTTPONLY', default=True):
-#     SESSION_COOKIE_HTTPONLY = True
-# if env('SESSION_COOKIE_SAMESITE', default=None):
-#     SESSION_COOKIE_SAMESITE = env('SESSION_COOKIE_SAMESITE')
+]
 
 # Celery Configuration
 CELERY_BROKER_URL = env('REDIS_URL')
